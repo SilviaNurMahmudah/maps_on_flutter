84282e8f89b5b62e34345e10b5912f4dd13197d0
SilviaNurMahmudah <silvia.mahmudah@gmail.com>
2022-09-14 22:09:09 +0700

    revisi

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _MyAppState createState() => _MyAppState();
 }
 
